5d51840488db14edfb24dac465e8c0c3098eab1b
SahnUzal <b.tahraoui.badr@gmail.com>
2026-02-05 12:48:58 -0500

    C headr For LibFt

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 11:56:25 by kali              #+#    #+#             */
+/*   Updated: 2026/02/05 12:42:12 by kali             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int		ft_isalpha(int c);
+int 	ft_isdigit(int c);
+int 	ft_isalnum(int c);
+int 	ft_isascii(int c);
+int 	ft_isprint(int c);
+int 	ft_strlen(const char *s);
+#endif
